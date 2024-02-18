5ede4cfdbb6d29511d15d70590447c11aa6961ee
Ayoub <ayoubzoubeidi@gmail.com>
2024-02-18 10:15:41 +0100

    Initial project commit

---
+++ src/main/java/com/example/subwaysandwichtruck/SubwaySandwichTruckApplication.java
@@ -0,0 +1,13 @@
+package com.example.subwaysandwichtruck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubwaySandwichTruckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubwaySandwichTruckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/subwaysandwichtruck/SubwaySandwichTruckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subwaysandwichtruck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubwaySandwichTruckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
